f17e13290504d0929fd550d0b1af7e9e44445716
Rizky Faisal <rizkyfaisal.career@gmail.com>
2026-03-21 01:21:22 +0700

    Update Struct.cpp

--- Struct.cpp
+++ Struct.cpp
@@ -13,7 +13,7 @@ struct profil{
 profil p[2] = {
         {"Rizky Faisal", "Olahraga", "S1 Teknik Komputer", 20}, 
         {"Angga Adi Pratama", "Olahraga", "S2 Ilmu Komunikasi", 21}     
-    };
+};
 
 for (int i = 0;i<2;i++){
     cout <<"\nProfil ke-"<< i+1 <<"\nnama = " << p[i].nama <<"\n"<<"Hobi = "<< p[i].hobi<<"\n"<<"Pendidikan = "<<p[i].pendidikan <<"\n"<<"Umur = "<< p[i].umur<<"\n";
