ec0f594d9455ce8982b151de89ed930d14d99c67
Rizky Faisal <rizkyfaisal.career@gmail.com>
2026-03-25 23:53:14 +0700

    Add files via upload

---
+++ kompleks1.cpp
@@ -0,0 +1,104 @@
+//25 Maret 2026
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum jenisbarang {ELEKTRONIK,PAKAIAN,MAKANAN};
+
+struct databarang{ 
+    string nama;
+    double harga;
+    int stock;
+    char kodeunik;
+};
+
+
+float totalnilaibarang;
+jenisbarang barang;
+
+int main (){
+
+int kategoribarang[3];
+int jumlahuser;
+
+cout << "Masukkan jumlah user yang ingin menginput = ";
+cin >> jumlahuser;
+cin.ignore();
+
+databarang jumlahdata [jumlahuser]; 
+cout << "\n";
+for (int i=0;i<jumlahuser;i++){
+    cout << "\n\tBarang ke-"<< i+1 <<"\n";
+    cout << "\n1. Elektronik\n2. Pakaian\n3. Makanan\n";
+    cout <<"Masukkan kategori barang = ";
+    cin >> kategoribarang[i];
+    cin.ignore();
+   
+    switch (kategoribarang[i]){
+    case 1:
+        barang = ELEKTRONIK;
+        if (barang == 0){
+            cout << "Barang dipilih = Barang Elektronik\n";
+        }else { cout << "Kode error!\n";}
+        break;
+
+    case 2: 
+        barang = PAKAIAN;
+        if (barang == 1){
+            cout << "Barang dipilih = Barang Pakaian\n";
+        } else { cout << "Kode error!\n";}
+        break;
+
+    case 3: 
+        barang = MAKANAN;
+        if (barang == 2){
+            cout << "Makanan\n";
+        } else { cout << "Kode error!\n";}
+        break;
+
+    default : 
+        cout << "\nBarang tidak tersedia!";
+    }
+
+    cout << "\nMasukkan nama barang = ";
+    getline (cin, jumlahdata[i].nama);
+
+    cout << "Masukkan Harga = ";
+    cin >> jumlahdata[i].harga;
+
+    cout << "Masukkan stock nya = ";
+    cin >> jumlahdata[i].stock;
+
+    cout << "Masukkan kode uniknya = ";
+    cin >> jumlahdata[i].kodeunik;
+
+    cin.ignore();
+
+     totalnilaibarang += jumlahdata[i].stock*jumlahdata[i].harga;
+}
+
+    cout <<"\nTotal nilai barang = " << totalnilaibarang << "\n";
+    cout << "\n\tData barang :\n";
+
+    for (int i = 0;i<jumlahuser;i++){
+        cout << "\n\tData barang ke-"<< i+1<<"\n";
+        cout << "Kategori yang dipilih pada barang ke-"<< i+1 <<"= " << jumlahdata[i].nama<< " dengan kode unik ("<< jumlahdata[i].kodeunik << ")\n";     
+    
+        cout << "stock barang = "<< jumlahdata[i].stock << "\n";
+        
+        if (jumlahdata[i].stock <5){
+            cout << "Kelangkaan = Barang langka!\n";
+        }else {
+            cout << "Kelangkaan = Tidak langka\n";
+        }
+        
+        if (jumlahdata[i].harga>100000){
+            cout << "Kemewahan = Barang mewah!\n";
+        }else {
+            cout << "Kemewahan = Barang biasa \n";
+        }
+    }
+
+
+    return 0;
+}
